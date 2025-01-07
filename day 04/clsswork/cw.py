9a7c501527cc5d43053a6958202d7f486a66097c
barbaredenozashvili <barbaredevnoza21@gmail.com>
2025-01-07 21:09:38 +0400

    Add files via upload

---
+++ day 04/clsswork/cw.py
@@ -0,0 +1,25 @@
+# 1 )საკლასო დავალება 
+#   შექმენით 4 ცვლადი სადაც        შეინახავთ string'ს
+#   შექმენით 4 ცვლადი სადაც        შეინახავთ integer'ს
+#   შექმენით 4 ცვლადი სადაც        შეინახავთ float'ს
+#   საბოლოოდ კი დაბეჭდეთ 12       იგივეს მნიშნველობა
+
+str1 = "Barbare"
+str2 = "Devnozashvili"
+str3 = "i love to eat cats"
+str4 = "becose i am chinese"
+
+int1 = 10 + 21 
+int2 = 1000 + 1
+int3 = 32 * 13
+int4 = 146 // 2
+
+float1 = 12.4
+float2 = 7.3
+float3 = 3.3
+float4 = 9.9
+
+print(str1, str2, str3, str4)
+print(int1, int2, int3, int4)
+print(float1, float2, float3, float4)
+
