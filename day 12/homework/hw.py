05e1ececf0bce2fd8650eced3477e591b156d64a
barbaredenozashvili <barbaredevnoza21@gmail.com>
2025-01-13 21:40:43 +0400

    Add files via upload

---
+++ day 12/homework/hw.py
@@ -0,0 +1,25 @@
+# int, str და float ბრძანებებზე ჩამოწერეთ 5-5 მაგალითი 
+
+
+a = 10  # მთლიან რიცხვად 10
+b = -5  # მთლიან რიცხვად -5
+c = 123456789  # დიდ მთლიან რიცხვად 123456789
+d = 0  # მთლიან რიცხვად 0
+e = 7 // 2  # სრულად გაყოფის შედეგი, 7 გადანაწილებულია 2-ზე (3)
+
+
+
+name = "John"  # სტრინგი "John"
+greeting = "Hello, World!"  # სტრინგი "Hello, World!"
+message = 'Python is awesome!'  # სტრინგი 'Python is awesome!'
+quote = "To be or not to be"  # სტრინგი "To be or not to be"
+empty_str = ""  # ცარიელი სტრინგი
+
+
+pi = 3.14159  # float ტიპი, პი 3.14159
+negative_float = -0.5  # მარცხენა ნიშანით მცირე რიცხვი
+large_float = 1.23e5  # მეცხრე ასეთის მიმოხილვის ნოტაცია (1.23 * 10^5)
+small_float = 0.00001  # პატარა float რიცხვი
+temperature = 36.6  # float ტიპი, სხეულის ტემპერატურა
+
+
